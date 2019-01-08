c005b50bca620e8e290f3e61964f8a9d5d30f17c
Dmitriy <spirin2205@yandex.ru>
2019-01-08 21:23:13 +0300

    Правки к защите

--- js/script.js
+++ js/script.js
@@ -19,7 +19,52 @@ let serviceContentCredit = document.querySelector(".services__content--credit");
 let allServicesBtn = document.querySelectorAll(".services__btn");
 let allServicesContent = document.querySelectorAll(".services__content");
 
+//slider variables
+let slidesBtn = document.querySelectorAll(".slider-nav-item");
+let slides = document.querySelectorAll(".slider__item");
+let firstSlide = document.querySelector(".slider__content--first");
+let secondSlide = document.querySelector(".slider__content--second");
+let thirdSlide = document.querySelector(".slider__content--third");
+let firstNavBtn = document.querySelector(".slider-nav-item--first");
+let secondNavBtn = document.querySelector(".slider-nav-item--second");
+let thirdNavBtn = document.querySelector(".slider-nav-item--third");
+
 ////////////////////////////////////////////////////////////////////
+
+//slider functions
+firstNavBtn.addEventListener("click", function(evt){
+    evt.preventDefault();
+    slidesBtn.forEach(function(evt){
+        evt.classList.remove("slider-nav-item--active");
+        slidesBtn[0].classList.add("slider-nav-item--active");
+    });
+    slides.forEach(function(evt){
+        evt.classList.add("d-none");
+        slides[0].classList.remove("d-none");
+    });   
+});
+secondNavBtn.addEventListener("click", function(evt){
+    evt.preventDefault();
+    slidesBtn.forEach(function(evt){
+        evt.classList.remove("slider-nav-item--active");
+        slidesBtn[1].classList.add("slider-nav-item--active");
+    });
+    slides.forEach(function(evt){
+        evt.classList.add("d-none");
+        slides[1].classList.remove("d-none");
+    });
+});
+thirdNavBtn.addEventListener("click", function(evt){
+    evt.preventDefault();
+    slidesBtn.forEach(function(evt){
+        evt.classList.remove("slider-nav-item--active");
+        slidesBtn[2].classList.add("slider-nav-item--active");
+    });
+    slides.forEach(function(evt){
+        evt.classList.add("d-none");
+        slides[2].classList.remove("d-none");
+    });
+});
 
 //popup functions
 
@@ -60,8 +105,10 @@ mapClose.addEventListener("click", function(evt){
 serviceBtnDelivery.addEventListener("click", function(evt){
     allServicesBtn.forEach(function(evt) {
         evt.classList.remove("services__btn--active");
+        evt.classList.add("services__btn");
     });
     serviceBtnDelivery.classList.add("services__btn--active");
+    serviceBtnDelivery.classList.remove("services__btn");
 
     allServicesContent.forEach(function(evt){
         evt.classList.add("visually-hidden")
@@ -73,9 +120,10 @@ serviceBtnDelivery.addEventListener("click", function(evt){
 serviceBtnAssurance.addEventListener("click", function(evt){
     allServicesBtn.forEach(function(evt) {
         evt.classList.remove("services__btn--active");
+        evt.classList.add("services__btn");
     });
     serviceBtnAssurance.classList.add("services__btn--active");
-    serviceContentAssurance.classList.remove("visually-hidden");
+    serviceBtnAssurance.classList.remove("services__btn");
 
     allServicesContent.forEach(function(evt){
         evt.classList.add("visually-hidden")
@@ -86,13 +134,13 @@ serviceBtnAssurance.addEventListener("click", function(evt){
 serviceBtnCredit.addEventListener("click", function(evt){
     allServicesBtn.forEach(function(evt) {
         evt.classList.remove("services__btn--active");
+        evt.classList.add("services__btn");
     });
     serviceBtnCredit.classList.add("services__btn--active");
-    serviceContentCredit.classList.remove("visually-hidden");
-
+    serviceBtnCredit.classList.remove("services__btn");
     allServicesContent.forEach(function(evt){
         evt.classList.add("visually-hidden")
     })
     serviceContentCredit.classList.remove("visually-hidden");
-})
+});
 
